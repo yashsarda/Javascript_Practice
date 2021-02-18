57032611f0d53c048f15612ffab2bdd6a250065b
Yash Sarda <yash1998sarda@gmail.com>
2021-02-18 17:50:29 +0530

    Creating classes with constructor function

---
+++ functions/constructor_functions.js
@@ -0,0 +1,26 @@
+function Range(from, to) {
+    this.from = from;
+    this.to = to;
+}
+
+Range.prototype = {
+
+    includes: function(x) {
+        return (x >= this.from && x <= this.to);
+    },
+
+    [Symbol.iterator]: function*() {
+        for (let x = Math.ceil(this.from); x <= this.to; ++x) {
+            yield x;
+        }
+    },
+
+    toString: function() {
+        return "(" + this.from + " " + this.to + ")";
+    }
+}
+
+let obj = new Range(1, 3);
+console.log(obj.includes(2));
+console.log(obj.toString());
+console.log([...obj]);
